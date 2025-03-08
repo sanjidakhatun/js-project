362f81c39dc69ddab8fcc27ba517b792a3670c2e
sanjidamukti <sanjidamukti147146@gmail.com>
2025-03-08 10:16:43 +0600

    Updated files for deployment

--- javascript.js
+++ javascript.js
@@ -33,82 +33,67 @@ fetch('./data.json')
                 }
             }
         });
-        
-        let selectedRoomType = 'all';
-        let guestsCapacity = 1;
-        let roomPrice = 0;
-        let roomSize = 0;
-        let Pet = null;
-        let Breakfast = null;
-
         filterData = () => {
-            // console.log('clicked');
-            
-            myData = data;
+            // Get the selected filter values
             selectedRoomType = document.getElementById('roomType').value;
             guestsCapacity = document.getElementById('guestsCapacity').value;
             roomPrice = document.getElementById('roomPrice').value;
             roomSize = document.getElementById('roomSize').value;
             Pet = document.getElementById('Pet').value;
             Breakfast = document.getElementById('Breakfast').value;
-            myData.forEach(e => {
-                if (selectedRoomType == 'all') {
+            
+            
+            let filteredRooms = []; 
+            
+            
+            data.forEach(e => {
+                if (selectedRoomType == 'all' || 
+                    (e.fields.type == selectedRoomType &&
+                     e.fields.capacity == guestsCapacity &&
+                     e.fields.price >= roomPrice &&
+                     e.fields.size >= roomSize)) {
+                    filteredRooms.push(e); 
+                }
+            });
+        
+    
+            rooms.innerHTML = ''; 
+        
+        
+            // If there are filtered rooms, display them
+            if (filteredRooms.length > 0) {
+                filteredRooms.forEach(room => {
                     rooms.insertAdjacentHTML("beforeend",
-                        `<div class="col-6 col-md-4">
-                            <div class="room">
-                                <div class="overlay">
-                                    <div class="image-container">
-                                    <img class="w-100" src="${e.fields.images[0].fields.file.url}">
-                                    <div class="price-top">
-                                        <h6>&dollar;${e.fields.price}</h6>
-                                        <p>per night</p>
-                                    </div>
-                                    </div>
-                                    <div class="overlay-button">
-                                    <div class="d-grid gap-2 col-6 mx-auto">
-                                        <a  class=" text-decoration-none my-btn" href="./details.html?slug=${e.fields.slug}">room details</a>
-                                    </div>
-                                    </div>
+                        `<div class="col-md-3 col-sm-6 mb-4">
+                            <div class="overlay">
+                            <div class="img-card">
+                                <div class="img position-relative">
+                                <img class="img-fluid" src="${room.fields.images[0].fields.file.url}">
+                                <div class="price-top">
+                                    <h6>&dollar;${room.fields.price}</h6>
+                                    <p>per night</p>
                                 </div>
-                                <p class="room-info">${e.fields.name}</p>
-                            </div>
-                        </div>`
-                    )
-                }
-                else if (e.fields.type == selectedRoomType && e.fields.capacity == guestsCapacity && e.fields.price >= roomPrice && e.fields.size >= roomSize ) {
-                    myData = e;
-                    console.log(myData);
-                    rooms.insertAdjacentHTML("beforeend",
-                        `<div class="col-12 col-md-4">
-                            <div class="room">
-                                <div class="overlay">
-                                    <div class="image-container">
-                                    <img class="w-100" src="${myData.fields.images[0].fields.file.url}">
-                                    <div class="price-top">
-                                        <h6>&dollar;${myData.fields.price}</h6>
-                                        <p>per night</p>
+                                    <div class="overlay-button position-absolute">
+                                      <a href="./feature1.html">
+                                       <button class="btn btn-info px-5 py-2">Feature</button>
+                                      </a>
+                                      </div>
                                     </div>
-                                    </div>
-                                    <div class="overlay-button">
-                                    <div class="d-grid gap-2 col-6 mx-auto">
-                                        <button class="btn btn-info text-uppercase my-btn p-2" type="button"><a href="./room-details.html?slug=${myData.fields.slug}">room details</a></button>
-                                    </div>
-                                    </div>
+                                   <p class="room-info">${room.fields.name}</p>
                                 </div>
-                                <p class="room-info">${myData.fields.name}</p>
                             </div>
-                        </div>`
-                    )
-                                       
-                }
-                else {
-                    console.log('else');
-                }
-                // console.log(myData);
-            })
-            // console.log(selectedRoomType);
+                            
+                        </div>`);
+                });
+            } else {
+                rooms.insertAdjacentHTML("beforeend", "<p>No rooms found matching your filters.</p>");
+            }
         }
-
-        filterData();
+        
+        
+         
+        
+        
+        
         
     })
